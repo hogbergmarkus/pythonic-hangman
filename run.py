743ad253ff87493e1b9845960b54eb7d9171a61b
Markus Högberg <m.o.hogberg@gmail.com>
2023-12-07 12:41:46 +0000

    Add function to handle end of game

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import random  # Random doc. https://www.w3schools.com/python/module_random.asp
+import sys
 from words import words
 
 
@@ -163,6 +164,23 @@ def lives(lives_left, guess, word):
         print("_____|_____")
 
 
+def end_of_game():
+    """
+    Handles end of game and user can choose to play again or not.
+    The exit function was found on stackoverflow:
+    https://stackoverflow.com/questions/40026651/how-to-stop-script-if-user-inputs-certain-characters-python
+    """
+    while True:
+        play_again = input("\nWant to play again? Y/N\n")
+        if play_again.upper() == "Y":
+            main()
+        elif play_again.upper() == "N":
+            print("Hope you had fun playing, see you next time!")
+            sys.exit()
+        else:
+            print(f"Select Y for yes, or N for no. You selected {play_again}")
+
+
 def main_game(word):
     """
     Main game loop.
@@ -186,11 +204,14 @@ def main_game(word):
             used_letters.append(guess)
             lives(lives_left, guess, word)
             lives_left -= 1
+
         if all(letter in correct_guesses for letter in word):
             print(f"Congratulations! You guessed the word: {word}")
-            break
+            end_of_game()
+
     if lives_left == 0:
         print(f"Sorry, you ran out of lives. The correct word was: {word}")
+        end_of_game()
 
 
 def main():
